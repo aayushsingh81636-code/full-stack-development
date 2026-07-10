140c46314ed6d78bae09a8ccd1b22881a97ad113
Aayush singh <aayushsingh81636@gmail.com>
2026-07-10 23:25:08 +0530

    multi line comments

--- JAVASCRIPT/first.js
+++ JAVASCRIPT/first.js
@@ -1,3 +1,6 @@
-a=5;//First number 
-b=5;//Second number
-console.log(a+b);//addition of two numbers
+/** 
+ * This is a simple JavaScript program to add two numbers
+ */
+ a = 5; // First number
+ b = 5; // Second number
+console.log(a + b); // Addition of two numbers
