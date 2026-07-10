b70ac0c2093fb7d2f1d03530a8df7ef72bc42d6d
Aayush singh <aayushsingh81636@gmail.com>
2026-07-10 23:32:04 +0530

    let keywords

--- JAVASCRIPT/first.js
+++ JAVASCRIPT/first.js
@@ -4,3 +4,8 @@
  a = 5; // First number
  b = 5; // Second number
 console.log(a + b); // Addition of two numbers
+/**
+ * Using the let keyword to declare a variable
+ */
+let c = 10; // Third number
+console.log(c); // Display the value of the third number
