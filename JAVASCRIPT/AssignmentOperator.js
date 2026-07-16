f2cea898c19299b729a7a433463fdcdd5b9b33c8
Aayush singh <aayushsingh81636@gmail.com>
2026-07-17 00:00:08 +0530

    Assignment operator

---
+++ JAVASCRIPT/AssignmentOperator.js
@@ -0,0 +1,8 @@
+let a=5;
+let b=10;
+console.log(a+=b);//Addition Assignment
+console.log(a-=b);//Subtraction Assignment
+console.log(a*=b);//Multiplication Assignment
+console.log(a/=b);//Division Assignment
+console.log(a%=b);//Modulus Assignment
+console.log(a**=b);//Exponentiation Assignment
