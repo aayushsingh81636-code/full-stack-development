b3e018456ec8551fb977f2e474a3ea1d14745759
Aayush singh <aayushsingh81636@gmail.com>
2026-07-17 00:09:56 +0530

    string concatenation

--- JAVASCRIPT/AssignmentOperator.js
+++ JAVASCRIPT/AssignmentOperator.js
@@ -6,3 +6,7 @@ console.log(a*=b);//Multiplication Assignment
 console.log(a/=b);//Division Assignment
 console.log(a%=b);//Modulus Assignment
 console.log(a**=b);//Exponentiation Assignment
+//String assignment operators
+let str1="Hello ";
+let str2="World";
+console.log(str1+=str2);//Concatenation Assignment
