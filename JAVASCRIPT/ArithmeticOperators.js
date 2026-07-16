2ffbc18d1bf4a5b33b5d96e3aa377d202e37e20d
Aayush singh <aayushsingh81636@gmail.com>
2026-07-16 23:53:49 +0530

    Modulus

--- JAVASCRIPT/ArithmeticOperators.js
+++ JAVASCRIPT/ArithmeticOperators.js
@@ -3,4 +3,5 @@ let b=5;
 console.log(a+b);//Addition 
 console.log(a-b);//Subtraction
 console.log(a*b);//Multiplication
-console.log(a*b);//Division
+console.log(a/b);//Division
+console.log(a%b);//Modulus
