3f0e13c1b24763ced96806281c79f36b0a7da301
Aayush singh <aayushsingh81636@gmail.com>
2026-07-17 10:19:23 +0530

    spread operator

---
+++ JAVASCRIPT/SpeedOperator.js
@@ -0,0 +1,5 @@
+let s="hello";
+console.log(...s);/*Spread operator is used to expand the iterables(array,string etc) into individual characters. 
+In this case, it will log each character of the string "hello" separately: h e l l o*/
+let arr=[1,2,3];
+console.log(...arr);
