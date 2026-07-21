073cafa3e76f54caece977d3b816e4186a325c49
Aayush singh <aayushsingh81636@gmail.com>
2026-07-21 13:13:55 +0530

    normal string

---
+++ JAVASCRIPT/String.js/normalstring.js
@@ -0,0 +1,5 @@
+let myString = "Hello, World!";
+console.log(myString);
+let myString2= 'Hello, JavaScript!';
+console.log(myString2);
+/**string is double quotes and single quotes are the same in JavaScript */
