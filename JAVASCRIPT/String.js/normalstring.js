b95053322d84e261f5fc4b64756821462356ea35
Aayush singh <aayushsingh81636@gmail.com>
2026-07-21 13:19:15 +0530

    template string

--- JAVASCRIPT/String.js/normalstring.js
+++ JAVASCRIPT/String.js/normalstring.js
@@ -1,5 +1,9 @@
+/**string is double quotes and single quotes are the same in JavaScript */
 let myString = "Hello, World!";
 console.log(myString);
 let myString2= 'Hello, JavaScript!';
 console.log(myString2);
-/**string is double quotes and single quotes are the same in JavaScript */
+/**template strings */
+let name = "Alice";
+let greeting = `Hello, ${name}!`;/**placed in backticks */
+console.log(greeting);
