3871905930b5f295ce510cb757c40ca0715f1309
Aayush singh <aayushsingh81636@gmail.com>
2026-07-22 09:33:41 +0530

    template string

---
+++ JAVASCRIPT/String.js/TemplateString.js
@@ -0,0 +1,5 @@
+let firstname = `Aayush`;
+let lastname = `Singh`;
+let fullname = firstname+lastname;
+console.log(fullname);
+/** This will log "AayushSingh" because the + operator concatenates the two strings */
