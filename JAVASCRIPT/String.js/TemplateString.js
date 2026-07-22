640e5071c8725d019e58fbd275fd104bffd070f2
Aayush singh <aayushsingh81636@gmail.com>
2026-07-22 09:44:23 +0530

    Interpolation of string

--- JAVASCRIPT/String.js/TemplateString.js
+++ JAVASCRIPT/String.js/TemplateString.js
@@ -1,5 +1,8 @@
-let firstname = `Aayush `;
-let lastname = `Singh's aim is to become a full stack developer.`;
-let fullname = firstname+lastname;
-console.log(fullname);
+let firstname = `Aayush`;
+let lastname = `Singh and my aim is to become a full stack developer.`;
+/*let fullname = firstname+lastname;*/
+   // console.log(fullname);
+   
 /** This will log "AayushSingh" because the + operator concatenates the two strings */
+/** Template literals allow for multi-line strings and string interpolation */
+console.log(`Hello,My name is ${firstname} ${lastname}`);
