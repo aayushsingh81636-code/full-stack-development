7b354714789eed21ed75fdb0952f8a3ef3673f67
Aayush singh <aayushsingh81636@gmail.com>
2026-07-22 09:36:55 +0530

    template string allows single and double quotes

--- JAVASCRIPT/String.js/TemplateString.js
+++ JAVASCRIPT/String.js/TemplateString.js
@@ -1,5 +1,5 @@
-let firstname = `Aayush`;
-let lastname = `Singh`;
+let firstname = `Aayush `;
+let lastname = `Singh is a "human being"`;
 let fullname = firstname+lastname;
 console.log(fullname);
 /** This will log "AayushSingh" because the + operator concatenates the two strings */
