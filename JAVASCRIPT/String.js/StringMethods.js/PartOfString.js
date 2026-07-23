6d0a5a51e58f840f772593c58ad2a4190a16f4bf
Aayush singh <aayushsingh81636@gmail.com>
2026-07-23 10:23:22 +0530

    substring function

--- JAVASCRIPT/String.js/StringMethods.js/PartOfString.js
+++ JAVASCRIPT/String.js/StringMethods.js/PartOfString.js
@@ -2,5 +2,9 @@ let str="There is a protest going on the jantar mantar in delhi city.";
 
 //str.slice(start, end)  method extracts a part of a string and returns the extracted part in a new string.
 
-console.log(str.slice(0, 5)); // "There"
-console.log(str.slice(10, 17)); // "protest"
+console.log(str.slice(-5)); 
+console.log(str.slice(10, 17)); 
+//str.substring(start, end) method extracts the characters from a string, 
+// between two specified indices and returns the new sub string.
+console.log(str.substring(7)); 
+ // "protest"
