d99581eedadcfde8099e87d5563df5d2f121c34f
Aayush singh <aayushsingh81636@gmail.com>
2026-07-23 10:16:31 +0530

    slice function

---
+++ JAVASCRIPT/String.js/StringMethods.js/PartOfString.js
@@ -0,0 +1,6 @@
+let str="There is a protest going on the jantar mantar in delhi city.";
+
+//str.slice(start, end)  method extracts a part of a string and returns the extracted part in a new string.
+
+console.log(str.slice(0, 5)); // "There"
+console.log(str.slice(10, 17)); // "protest"
