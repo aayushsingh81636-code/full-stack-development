d7ee313debc18c37e17fbc8206ce638178fab6bc
Aayush singh <aayushsingh81636@gmail.com>
2026-07-23 10:03:17 +0530

    ..

--- JAVASCRIPT/String.js/StringMethods.js/StringAccessing.js
+++ JAVASCRIPT/String.js/StringMethods.js/StringAccessing.js
@@ -1,11 +1,16 @@
 let str="";
+
 //charAt() method returns the character at a specified index (position) in a string:
 console.log(str.charAt(0));
+
 //charCodeAt() method returns the Unicode of the character at a specified index in a string:
 console.log(str.charCodeAt(0));
+
 /*at() method returns the character at a specified index (position) in a string:
 it supports negative indices too and it is new to ES2022*/
 console.log(str.at(0));
 console.log(str.at(-4));
+
 /**property access might be unpredictable */
 console.log(str[0]);
+
