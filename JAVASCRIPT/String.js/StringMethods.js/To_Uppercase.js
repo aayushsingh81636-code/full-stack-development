ded3dfabb836e842a7de95011fb3d1c404768cc1
Aayush singh <aayushsingh81636@gmail.com>
2026-07-25 19:49:05 +0530

    lowercase and uppercase

---
+++ JAVASCRIPT/String.js/StringMethods.js/To_Uppercase.js
@@ -0,0 +1,8 @@
+//touppercase()
+let a = "aayush singh"
+let b=a.toUpperCase();
+console.log("uppercase=",b);
+//tolowercase()
+let c="AAYUSH SINGH";
+let d=c.toLowerCase();
+console.log("lowercase=",d);
