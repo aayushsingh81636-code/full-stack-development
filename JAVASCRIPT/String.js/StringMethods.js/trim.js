c36d7c92fc075cc9662a620ecd5bfd23f06e69b5
Aayush singh <aayushsingh81636@gmail.com>
2026-07-26 23:58:49 +0530

    trim method

---
+++ JAVASCRIPT/String.js/StringMethods.js/trim.js
@@ -0,0 +1,11 @@
+//trim method replace the whitespace characters of both sides of the string
+let a="     Aayush singh     ";
+let b=a.trim();
+console.log(b);
+//trimStart method replace the whitespaces from the start only
+let c=a.trimStart();
+console.log(c);
+//trimEnd method replace the whitespaces form the end only
+let d="      aoehpoawpoje      ";
+let e=d.trimEnd();
+console.log(e);
