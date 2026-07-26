0dd555f7a7a8a168574b2d02899318b9f0a97c10
Aayush singh <aayushsingh81636@gmail.com>
2026-07-26 23:53:10 +0530

    toWELLPERFORMED FUNCTON

--- JAVASCRIPT/String.js/StringMethods.js/iswellperformed.js
+++ JAVASCRIPT/String.js/StringMethods.js/iswellperformed.js
@@ -1,7 +1,10 @@
 let a="Aayush singh";
 let b=a.isWellFormed();
 console.log(b);
-//
+//if a string is contains lone surrogates then that string is not well performed
 let c="Aayush singh \uD800";
 let d=c.isWellFormed();
 console.log(d);
+// toWELLPERFORMED() FUNCTION replace the lone surrogates with the unicode replacement character
+let e=c.toWellFormed();
+console.log(e);
