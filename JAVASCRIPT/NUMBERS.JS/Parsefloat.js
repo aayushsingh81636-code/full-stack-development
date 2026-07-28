45f0fbb1ff0b316a29566184b1bcc73aa99eeacf
Aayush singh <aayushsingh81636@gmail.com>
2026-07-28 15:27:30 +0530

    parsefloat function

---
+++ JAVASCRIPT/NUMBERS.JS/Parsefloat.js
@@ -0,0 +1,9 @@
+//parseFloat() parses a string and returns a number. Spaces are allowed. Only the first number is returned
+let a="Aayush singh"
+let b="34";
+let c="10 20 30";
+let d="12.333";
+console.log(parseFloat(a));
+console.log(parseFloat(b));
+console.log(parseFloat(c));
+console.log(parseFloat(d));
