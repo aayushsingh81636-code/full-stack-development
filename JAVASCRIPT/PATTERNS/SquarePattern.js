d8ec113e4982f874c14ea4b185f2c4ed7285c2f7
Aayush singh <aayushsingh81636@gmail.com>
2026-08-01 21:50:57 +0530

    square pattern of the alphabets

--- JAVASCRIPT/PATTERNS/SquarePattern.js
+++ JAVASCRIPT/PATTERNS/SquarePattern.js
@@ -6,3 +6,14 @@ for(let i=1;i<=5;i++){
 }
 //console.log("\n"); // Add an extra newline for better readability that's why we are using " process.stdout.write("\n") " instead of 
 // console.log("\n")
+
+/*A B C D
+  A B C D
+  A B C D
+  A B C D*/
+  for(var  a=1;a<=4;a++){
+    for( var b=1;b<=4;b++){
+        process.stdout.write(String.fromCharCode(64+b) + " "); 
+    }
+    process.stdout.write("\n");
+  }
--- JAVASCRIPT/STRING/StringMethods.js/StringAccessing.js
+++ JAVASCRIPT/STRING/StringMethods.js/StringAccessing.js
@@ -1,10 +1,10 @@
-let str="";
+let str="Aayush";
 
 //charAt() method returns the character at a specified index (position) in a string:
 console.log(str.charAt(0));
 
 //charCodeAt() method returns the Unicode of the character at a specified index in a string:
-console.log(str.charCodeAt(65));
+console.log(str.charCodeAt(4));
 
 /*at() method returns the character at a specified index (position) in a string:
 it supports negative indices too and it is new to ES2022*/
