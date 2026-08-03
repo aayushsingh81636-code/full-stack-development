62813e78beb796633cf87eda4fb9c37c6e332a40
Aayush singh <aayushsingh81636@gmail.com>
2026-08-03 23:56:28 +0530

    padStart method

---
+++ JAVASCRIPT/STRING/StringMethods.js/padStartandpadEnd.js
@@ -0,0 +1,6 @@
+let str = "Hello";
+console.log(str.padStart(10, "k"));//The padStart() method pads a string from the start.
+//It pads a string with another string (multiple times) until it reaches a given length.
+let number=123;
+let str1=number.toString();
+console.log(str.padStart(10, "0"));//The padStart() method pads a string from the start.
