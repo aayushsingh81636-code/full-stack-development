d4f0b3ed9480af8acabdff82a805d6c80de7c074
Aayush singh <aayushsingh81636@gmail.com>
2026-08-04 18:51:01 +0530

    repeat method of string

---
+++ JAVASCRIPT/STRING/StringMethods.js/repeatmethod.js
@@ -0,0 +1,4 @@
+let a="Hello";
+let b=a.repeat(3);
+console.log(b);//The repeat() method constructs and returns a new string which contains the specified
+            // number of copies of the string on which it was called, concatenated together.
