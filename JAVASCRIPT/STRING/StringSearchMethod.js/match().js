3410474b8000f50b1fd2498545099c39409c9b74
Aayush singh <aayushsingh81636@gmail.com>
2026-08-08 23:56:26 +0530

    match method of javascript

---
+++ JAVASCRIPT/STRING/StringSearchMethod.js/match().js
@@ -0,0 +1,7 @@
+/*let a="I just want to learn javascript and I want to Learn it well";
+let b=a.match("want");
+console.log(b);
+*/
+let a="I just want to learn javascript and I want to Learn it well";
+let b=a.match(/want/g);
+console.log(b);
