7e271d6409e469ce668f561a63c5ffa6fd955fdb
Aayush singh <aayushsingh81636@gmail.com>
2026-08-09 00:00:00 +0530

    math regular expression

--- JAVASCRIPT/STRING/StringSearchMethod.js/match().js
+++ JAVASCRIPT/STRING/StringSearchMethod.js/match().js
@@ -1,7 +1,13 @@
-/*let a="I just want to learn javascript and I want to Learn it well";
+//The match() method returns an array containing the results of matching a string against a string (or a regular expression).
+let a="I just want to learn javascript and I want to Learn it well";
 let b=a.match("want");
 console.log(b);
-*/
+
+//perform a global search for the word "want" in the string and return all occurrences
 let a="I just want to learn javascript and I want to Learn it well";
 let b=a.match(/want/g);
+console.log(b);
+//perform a case-insensitive search for the word "want" in the string and return all occurrences
+let a="I just want to learn javascript and I WANT to Learn it well";
+let b=a.match(/want/gi);
 console.log(b);
